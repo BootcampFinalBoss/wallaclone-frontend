c57a353a25c46cb7789186a01925c3ef75df2481
Jose Signoret <jsignoretfernandez@gmail.com>
2021-03-06 17:04:24 +0100

    Less Changes

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,3 +1,4 @@
-export { default as RegisterPage } from './RegisterPage';
-export { default as LoginPage } from './LoginPage';
-export {default as ForgotPasswordPage} from './ForgotPasswordPage';
+export { default as RegisterPage } from "./RegisterPage";
+export { default as LoginPage } from "./LoginPage";
+export { default as ForgotPasswordPage } from "./ForgotPasswordPage";
+export { default as ResetPassword } from "./ResetPassword";
