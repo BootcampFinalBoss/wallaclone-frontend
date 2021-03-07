514a70a98b3dea9c192a856cb7e32d3cc3c51f68
Kiara <kiaraymg@gmail.com>
2021-03-07 09:29:44 +0100

    Fixes on AdvertsList.

--- src/components/adverts/AdvertsList.js
+++ src/components/adverts/AdvertsList.js
@@ -5,15 +5,15 @@ import AdvertCard from './AdvertCard';
 
 const { Title } = Typography;
 
-const AdvertsList = ({ adverts }) => {
+const AdvertsList = ({ adverts, ui }) => {
   return (
     <Col xs={20} lg={16} className="adverts">
       <Title type="h2" className="adverts__title text-center my-4">
         Adverts List
       </Title>
       <Row gutter={[24, 24]}>
-        {adverts &&
-          adverts.map((ad) => {
+        {!ui.loading &&
+          adverts?.map((ad) => {
             return <AdvertCard key={ad._id} ad={ad} checkDetail={true} />;
           })}
       </Row>
