5675d213dabeff2cd930e4e105664d7c204bb195
Kiara <kiaraymg@gmail.com>
2021-03-07 21:56:55 +0100

    Fix.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { getUi } from "../../store/selectors";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getUi } from '../../store/selectors';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,7 +8,7 @@ import {
   Redirect,
   useLocation,
   useHistory,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 import {
   PrivateRoute,
@@ -28,18 +28,17 @@ import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 import { Content } from 'antd/lib/layout/layout';
 import CommonErrorPage from '../errors/CommonErrorPage';
-} from "../auth";
-import { UserProfile } from "../users";
+import { UserProfile } from '../users';
 
 const App = () => {
   const location = useLocation();
   const ui = useSelector((state) => getUi(state));
   const history = useHistory();
-  let HideHeader = location.pathname.match("/login") ? null : <Header />;
-  let HideFooter = location.pathname.match("/login") ? null : <Footer />;
+  let HideHeader = location.pathname.match('/login') ? null : <Header />;
+  let HideFooter = location.pathname.match('/login') ? null : <Footer />;
 
   if (ui.error) {
-    history.push("/error");
+    history.push('/error');
   }
 
   return (
